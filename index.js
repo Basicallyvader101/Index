0dc8d7a647cc90e078558975f0c6d2fda115c00d
Basicallyvader101 <erin.hansen1125@gmail.com>
2026-03-20 01:23:30 -0500

    burger button for mobile

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function openMenu() {
-    document.body.classList += " menu--open";
+    document.body.classList.add("menu--open");
 }
+
 function closeMenu() {
     document.body.classList.remove("menu--open");
 }
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+        closeMenu();
+    }
+});
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 1024) {
+        closeMenu();
+    }
+});
